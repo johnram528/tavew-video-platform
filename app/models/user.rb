d1f2e51001999d921f64fa8f180011e8c308391e
johnram528 <johnram528@gmail.com>
2017-05-24 20:39:19 -0400

    uncommented user model and added seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 #User model is pending
 
-# class User < ApplicationRecord
-#   has_many :videos 
+class User < ApplicationRecord
+  has_many :videos 
 
-#   validates_presence_of :username, :email  
-#   validates_uniqueness_of :username, :email
-#   has_secure_password
-#   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-# end
+  validates_presence_of :username, :email  
+  validates_uniqueness_of :username, :email
+  has_secure_password
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(username: 'johnram528', email: 'johnram528@gmail.com', password:'password')
+
+file1 = File.open('/Users/master/Downloads/Girl_Checks_Phone.mp4')
+file2 = File.open('/Users/master/Downloads/uscenes_h-264_hd_test.mp4')
+file3 = File.open('/Users/master/Downloads/mov_bbb.mp4')
+
+user.videos.create(title: 'test1', description: 'test1', file: file1)
+user.videos.create(title: 'test2', description: 'test2', file: file2)
+user.videos.create(title: 'test3', description: 'test3', file: file3)
